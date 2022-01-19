564712f941b54ff0a673038613408f14958ba20e
namit1203 <97530690+namit1203@users.noreply.github.com>
2022-01-20 01:30:13 +0700

    Create ws33.cpp

---
+++ ws33.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int m,n;
+    scanf("%d%d",&m,&n);
+    int r,i,luu;
+    for(i = m;i <= n;i++){
+        luu=i;
+        int sum=0;;
+        while(luu){
+            r = luu % 10; 
+            sum = (sum*10 ) + r;
+            luu = luu/10;
+            
+        }
+        if(i==sum){
+            printf("%d ",i);
+        }
+}
+}
